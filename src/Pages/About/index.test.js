a201239292a4ed790545049aa99d9e967821baa9
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-04-04 21:12:37 +0800

    Removed About and Portfolio pages

--- src/Pages/About/index.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import About from './index.js';
-
-test('renders This is my about page.', () => {
-  render(<About />);
-  const linkElement = screen.getByText(/This is my about page./i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/Pages/Portfolio/index.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Portfolio from './index.js';
-
-test('renders This is my portfolio page.', () => {
-  render(<Portfolio />);
-  const linkElement = screen.getByText(/This is my portfolio page./i);
-  expect(linkElement).toBeInTheDocument();
-});
