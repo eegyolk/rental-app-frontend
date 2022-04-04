1c1006e633a91231f3a90873f055c3a53082faad
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-04-04 21:14:40 +0800

    Updated reducer setup

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,19 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
-import aboutReducer from '../Pages/About/reducer';
-import portfolioReducer from '../Pages/Portfolio/reducer';
+import homeReducer from '../Pages/Home/reducer';
+import searchReducer from '../Pages/Search/reducer';
+import renterReducer from '../Pages/Renter/reducer';
+import agentReducer from '../Pages/Agent/reducer';
+import landlordReducer from '../Pages/Landlord/reducer';
 import { watcherSaga } from './rootSaga';
 
 const reducers = combineReducers({
-  about: aboutReducer,
-  portfolio: portfolioReducer,
+  home: homeReducer,
+  search: searchReducer,
+  renter: renterReducer,
+  agent: agentReducer,
+  landlord: landlordReducer,
 });
 
 const sagaMiddleware = createSagaMiddleware();
