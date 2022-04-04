7d783645fbd67fdfde6281456566fb7e94ffd145
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-04-04 21:14:22 +0800

    Removed default setup in watcherSaga

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,7 +1,1 @@
-import { takeLatest } from 'redux-saga/effects';
-import { GET_USER } from '../Pages/Portfolio/reducer/actionTypes';
-import { handleGetUser } from '../Pages/Portfolio/saga/handlers';
-
-export function* watcherSaga() {
-  yield takeLatest(GET_USER, handleGetUser);
-}
+export function* watcherSaga() {}
