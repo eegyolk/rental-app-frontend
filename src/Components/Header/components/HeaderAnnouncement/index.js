d308058834e0a5bd1ba7f8488bd62f7a73049324
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-04-06 23:19:41 +0800

    Updated HeaderAnnouncment component, implemented charkra-ui

--- src/Components/Header/components/HeaderAnnouncement/index.js
+++ src/Components/Header/components/HeaderAnnouncement/index.js
@@ -1,8 +1,15 @@
+import { Box, HStack, Text } from '@chakra-ui/react';
+
 function HeaderAnnouncement() {
   return (
-    <div>
-      Announcement here / For the latest updates, please visit our website...
-    </div>
+    <Box bg="#E1E1E1" w="100%" p={3} color="black">
+      <HStack spacing={2}>
+        <Text fontSize="md">COVID-19 INFORMATION / </Text>
+        <Text fontSize="sm">
+          For the latest updates, please visi our coronavirs (COVID-19) website
+        </Text>
+      </HStack>
+    </Box>
   );
 }
 
