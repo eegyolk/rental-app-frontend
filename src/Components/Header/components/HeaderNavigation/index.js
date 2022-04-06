723b7bd8818ba5d6d660932126c787bd79ec028c
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-04-06 22:22:07 +0800

    Removed Outlet here

--- src/Components/Header/components/HeaderNavigation/index.js
+++ src/Components/Header/components/HeaderNavigation/index.js
@@ -1,34 +1,26 @@
-import { Link, Outlet } from 'react-router-dom';
-
-import './style.css';
+import { Link } from 'react-router-dom';
 
 function HeaderNavigation() {
   return (
-    <div>
-      <nav>
-        <ul className="HeaderNavigation-ul">
-          <li className="HeaderNavigation-li">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="HeaderNavigation-li">
-            <Link to="/search">Search</Link>
-          </li>
-          <li className="HeaderNavigation-li">
-            <Link to="/renter">For renters</Link>
-          </li>
-          <li className="HeaderNavigation-li">
-            <Link to="/agent">For agents</Link>
-          </li>
-          <li className="HeaderNavigation-li">
-            <Link to="/landlord">For landlords</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
-
-      <Outlet />
-    </div>
+    <nav>
+      <ul className="HeaderNavigation-ul">
+        <li className="HeaderNavigation-li">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="HeaderNavigation-li">
+          <Link to="/search">Search</Link>
+        </li>
+        <li className="HeaderNavigation-li">
+          <Link to="/renter">For renters</Link>
+        </li>
+        <li className="HeaderNavigation-li">
+          <Link to="/agent">For agents</Link>
+        </li>
+        <li className="HeaderNavigation-li">
+          <Link to="/landlord">For landlords</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
