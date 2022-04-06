cb8901285298ae956cc5aa686d4665bbccbffd40
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-04-06 22:22:25 +0800

    Removed routes here

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,21 +1,12 @@
-import { useRoutes } from 'react-router-dom';
-
-import headerNavigationRoutes from '../../routes/headerNavigationRoutes';
 import HeaderAnnouncement from './components/HeaderAnnouncement';
+import HeaderNavigation from './components/HeaderNavigation';
 
 function Header() {
-  const routes = useRoutes(headerNavigationRoutes);
-
   return (
-    <div>
-      <div>
-        <HeaderAnnouncement />
-      </div>
-      <div>
-        {/* LOGO HERE */}
-        {routes}
-      </div>
-    </div>
+    <header>
+      <HeaderAnnouncement />
+      <HeaderNavigation />
+    </header>
   );
 }
 
