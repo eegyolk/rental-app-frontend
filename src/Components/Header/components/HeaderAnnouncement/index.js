d5d7150c040eb3b696b20e1e658fdeae1dbaea25
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-04-12 23:01:06 +0800

    Used Box componenent instead of div element

--- src/Components/Header/components/HeaderAnnouncement/index.js
+++ src/Components/Header/components/HeaderAnnouncement/index.js
@@ -4,9 +4,11 @@ function HeaderAnnouncement() {
   return (
     <Box bg="#E1E1E1" w="100%" p={3} color="black">
       <HStack spacing={2}>
-        <Text fontSize="md">COVID-19 INFORMATION / </Text>
-        <Text fontSize="sm">
-          For the latest updates, please visi our coronavirs (COVID-19) website
+        <Text fontSize="md" fontWeight="semibold">
+          COVID-19 INFORMATION /{' '}
+        </Text>
+        <Text fontSize="sm" fontWeight="light">
+          For the latest updates, please visit our coronavirs (COVID-19) website
         </Text>
       </HStack>
     </Box>
