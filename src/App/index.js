14486451cc22aba6f1d635f55d6e8dc33016aa8e
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-04-12 23:00:07 +0800

    Use Box component instead of div html elemenet

--- src/App/index.js
+++ src/App/index.js
@@ -1,3 +1,4 @@
+import { Box } from '@chakra-ui/react';
 import { useRoutes } from 'react-router-dom';
 
 import headerNavigationRoutes from '../routes/headerNavigationRoutes';
@@ -5,7 +6,11 @@ import headerNavigationRoutes from '../routes/headerNavigationRoutes';
 function App() {
   const routes = useRoutes(headerNavigationRoutes);
 
-  return <div>{routes}</div>;
+  return (
+    <Box w="100%" textAlign="center">
+      {routes}
+    </Box>
+  );
 }
 
 export default App;
