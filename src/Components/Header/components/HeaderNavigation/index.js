a963c09a26ec0f33d31aeb4b5f9a58ae36eb90df
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-04-12 23:01:43 +0800

    Updated layout of header nav

--- src/Components/Header/components/HeaderNavigation/index.js
+++ src/Components/Header/components/HeaderNavigation/index.js
@@ -1,25 +1,43 @@
-import { Box, HStack, Link } from '@chakra-ui/react';
+import { Box, Spacer, HStack, Link, Image, Text } from '@chakra-ui/react';
 import { Link as ReachLink } from 'react-router-dom';
+
+import logoHeader from '../../../../assets/images/logo-header.png';
 
 function HeaderNavigation() {
   return (
     <nav>
-      <Box bg="#E46D2A" w="100%" p={6} color="white">
-        <HStack spacing={2}>
+      <Box bg="#E46D2A" w="100%" p={5} color="white" display="flex">
+        <HStack spacing={10}>
           <Link as={ReachLink} to="/">
-            Home
+            <Image src={logoHeader} alt="Balai Header Logo" />
           </Link>
           <Link as={ReachLink} to="/search">
-            Search
+            <Text fontWeight="bold">Search</Text>
           </Link>
           <Link as={ReachLink} to="/renter">
-            For renters
+            <Text fontWeight="bold">For renters</Text>
           </Link>
           <Link as={ReachLink} to="/agent">
-            For agents
+            <Text fontWeight="bold">For agents</Text>
           </Link>
           <Link as={ReachLink} to="/landlord">
-            For landlords
+            <Text fontWeight="bold">For landlords</Text>
+          </Link>
+        </HStack>
+        <Spacer />
+        <HStack spacing={10}>
+          <Link as={ReachLink} to="/signin">
+            <Text fontWeight="bold">Sign In</Text>
+          </Link>
+          <Link
+            as={ReachLink}
+            to="/list-property"
+            border="1px"
+            borderRadius={5}
+            borderStyle="solid"
+            p={3}
+          >
+            <Text fontWeight="bold">List property</Text>
           </Link>
         </HStack>
       </Box>
