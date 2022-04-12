2463e71ffb598788f11fc51089aabd87bd46fd07
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-04-12 23:00:36 +0800

    Used Box componenent instead of div element

--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -1,11 +1,13 @@
+import { Box } from '@chakra-ui/react';
+
 function Banner() {
   return (
-    <div>
+    <Box bg="#E46D2A" w="100%" h={100} p={3} color="black">
       <div>100% Refund Guarantee</div>
       <div>
         If what you see is not what you get, we will refund 100% back to you.
       </div>
-    </div>
+    </Box>
   );
 }
 
