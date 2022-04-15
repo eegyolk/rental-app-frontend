315840b72d14fa67bbb777ff965e7ac849154228
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-04-16 00:22:37 +0800

    Implemented new child components and isHome props

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -5,29 +5,60 @@ import SearchByAreaOrProperty from './components/SearchByAreaOrProperty';
 import SearchByPropertyType from './components/SearchByPropertyType';
 import SearchFilter from './components/SearchFilter';
 import SearchTags from './components/SearchTags';
+import SearchByPreferences from './components/SearchByPreferences';
+import SearchByFeatures from './components/SearchByFeatures';
+import SearchByAmenities from './components/SearchByAmenities';
 
-function Search() {
+function Search(props) {
+  const { isHome } = props;
+
   return (
-    <Box
-      bg="whiteAlpha.900"
-      h="50px"
-      m="40px"
-      color="white"
-      backgroundSize="100% 100%"
-      position="relative"
-      borderRadius={5}
-      display="block"
-    >
-      <VStack spacing={4} align="stretch">
-        <HStack>
-          <SearchByAreaOrProperty />
-          <SearchByPropertyType />
-          <SearchFilter />
-        </HStack>
-        <HStack>
-          <SearchTags />
-        </HStack>
-      </VStack>
+    <Box>
+      {(isHome && (
+        <Box
+          bg="whiteAlpha.900"
+          h="50px"
+          m="40px"
+          color="white"
+          backgroundSize="100% 100%"
+          position="relative"
+          borderRadius={5}
+          display="block"
+        >
+          <VStack spacing={4} align="stretch">
+            <HStack>
+              <SearchByAreaOrProperty />
+              <SearchByPropertyType />
+              <SearchFilter />
+            </HStack>
+            <HStack>
+              <SearchTags />
+            </HStack>
+          </VStack>
+        </Box>
+      )) || (
+        <Box
+          bg="whiteAlpha.900"
+          h="50px"
+          color="white"
+          backgroundSize="100% 100%"
+          position="relative"
+          display="block"
+        >
+          <VStack spacing={0} align="stretch">
+            <HStack>
+              <SearchByAreaOrProperty isHome={isHome} />
+              <SearchByPropertyType />
+              <SearchFilter isHome={isHome} />
+            </HStack>
+            <HStack spacing={0} textAlign="justify">
+              <SearchByPreferences />
+              <SearchByFeatures />
+              <SearchByAmenities />
+            </HStack>
+          </VStack>
+        </Box>
+      )}
     </Box>
   );
 }
